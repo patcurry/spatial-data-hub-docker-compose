4e970efc941163620685c1cffdd0ceed344273b8
patcurry <currypat1985@gmail.com>
2019-02-22 14:41:17 +0100

    changed up the environmental variables

--- app/spatialdatahub.org/main/settings/production.py
+++ app/spatialdatahub.org/main/settings/production.py
@@ -24,18 +24,3 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SESSION_EXPIRE_AT_BROWSER_CLOSE=True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-
-if "RDS_HOSTNAME" in os.environ:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.environ["RDS_DB_NAME"],
-            "USER": os.environ["RDS_USERNAME"],
-            "PASSWORD": os.environ["RDS_PASSWORD"],
-            "HOST": os.environ["RDS_HOSTNAME"],
-            "PORT": os.environ["RDS_PORT"],
-        }
-    }
-
-
-#STATIC_URL = "https://s3.eu-central-1.amazonaws.com/spatialdatahub-static/"
